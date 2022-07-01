9f0edc11738f5845a8333281e28a6a174c31cb9f
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-07-01 09:59:01 +0800

    Add: lang set menu

---
+++ src/components/SettingList/LangSet/index.tsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+const LangSet = () => {
+  return (
+    <div className="lang-menu">
+      LangSet
+    </div>
+  )
+}
+
+export default LangSet
--- src/components/SettingList/index.tsx
+++ src/components/SettingList/index.tsx
@@ -1,12 +1,18 @@
 import './style.scss'
 import langImg from '../../images/lang.png'
+import LangSet from './LangSet'
+import { useState } from 'react'
 
 const SettingList = () => {
+  const [langMenuShow, setLangMenuShow] = useState(false)
+
   return (
     <div className='setting-list'>
-      <div className='setting-item lang-set'>
+      <div className='setting-item lang-set' onClick={() => setLangMenuShow(true)}>
         <img src={langImg} alt="" className='lang-img' />
       </div>
+      {langMenuShow ? <LangSet /> : null}
+      {langMenuShow ? <div className='black-back' onClick={() => setLangMenuShow(false)}></div> : null}
     </div>
   )
 }
